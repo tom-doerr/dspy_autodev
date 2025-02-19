9e17978861f6d3ced1dfa74aa382491b6105a019
Tom Dörr <tomdoerr96@gmail.com>
2025-02-19 04:22:07 +0100

    .

--- dspy-pipeline/tests/test_main.py
+++
@@ -1,6 +0,0 @@
-import os
-import pytest
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from dspy_pipeline.fix_instruction_generator import FixInstructionGenerator
-from dspy_pipeline.fix_applier import FixApplier
--- path/dspy-pipeline/tests/test_main.py
+++
@@ -1,4 +0,0 @@
-import os
-import pytest
-from dspy_pipeline.fix_instruction_generator import FixInstructionGenerator
-from dspy_pipeline.fix_applier import FixApplier
